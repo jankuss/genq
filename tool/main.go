6791683552d34123b281b42b161ed0d6c2291d80
Jan <jan@kuss.dev>
2024-04-02 20:34:20 +0200

    Fix formatting

--- tool/main.go
+++ tool/main.go
@@ -14,18 +14,18 @@ var (
 
 // main with arguments
 func main() {
-  for _, arg := range os.Args {
-    if arg == "--version" {
-      fmt.Println("=========================================================")
-      fmt.Println("genq - Blazingly fast code generator for Dart and Flutter")
-      fmt.Println("=========================================================")
+	for _, arg := range os.Args {
+		if arg == "--version" {
+			fmt.Println("=========================================================")
+			fmt.Println("genq - Blazingly fast code generator for Dart and Flutter")
+			fmt.Println("=========================================================")
 
-      fmt.Println("version:", version)
-      fmt.Println("commit:", commit)
-      fmt.Println("date:", date)
-      os.Exit(0)
-    }
-  }
+			fmt.Println("version:", version)
+			fmt.Println("commit:", commit)
+			fmt.Println("date:", date)
+			os.Exit(0)
+		}
+	}
 
 	cli.Execute()
 }
