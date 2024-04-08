8e10e118fbc0a8713374ea4d8790b0f657960af5
Jan <jan@kuss.dev>
2024-04-08 23:57:57 +0200

    Improve documentation

--- packages/genq/lib/src/annotation.dart
+++ packages/genq/lib/src/annotation.dart
@@ -6,10 +6,8 @@ const genq = Genq();
 
 /// Annotation to mark a class for code generation with genq. If you don't have
 /// to supply any options, you can use the top-level [genq] annotation instead.
-///
-/// Options:
-/// [json] - Wether to generate a toJson/fromJson method for the class.
 class Genq {
+  /// Whether or not there should be ToJson and FromJson generated for this class.
   final bool json;
 
   const Genq({
@@ -23,21 +21,30 @@ class GenqJsonEnum {
 }
 
 class JsonKey {
+  /// The key in the JSON map that corresponds to this field.
   final String? name;
+  /// A custom function to use when decoding this field.
   final Function? fromJson;
+  /// A custom function to use when encoding this field.
   final Function? toJson;
+  /// When decoding an enum, the value to use when the value does not match any enums.
   final Enum? unknownEnumValue;
+  /// The default value to use when the JSON value is `null`.
+  final Object? defaultValue;
 
   const JsonKey({
     this.name,
     this.unknownEnumValue,
     this.fromJson,
     this.toJson,
+    this.defaultValue,
   });
 }
 
+/// Annotation to specify how an enum should be serialized.
 class JsonValue {
-  final Object value;
+  /// The value to use when serializing this enum.
+  final dynamic value;
 
   const JsonValue(this.value);
 }
