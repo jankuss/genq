f4b6ae2cd856fb3d688e11e5a58a62187579bf05
Jan <jan@kuss.dev>
2024-04-25 23:05:47 +0200

    Add default annotation

--- packages/genq/lib/src/annotation.dart
+++ packages/genq/lib/src/annotation.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta_meta.dart';
+
 export 'package:collection/collection.dart' show DeepCollectionEquality;
 
 /// Annotation to mark a class for code generation with genq. If you need to
@@ -6,6 +8,7 @@ const genq = Genq();
 
 /// Annotation to mark a class for code generation with genq. If you don't have
 /// to supply any options, you can use the top-level [genq] annotation instead.
+@Target({TargetKind.classType})
 class Genq {
   /// Whether or not there should be ToJson and FromJson generated for this class.
   final bool json;
@@ -16,19 +19,25 @@ class Genq {
 }
 
 /// Annotation to mark an enum for JSON serialization code generation with genq.
+@Target({TargetKind.enumType})
 class GenqJsonEnum {
   const GenqJsonEnum();
 }
 
+@Target({TargetKind.parameter})
 class JsonKey {
   /// The key in the JSON map that corresponds to this field.
   final String? name;
+
   /// A custom function to use when decoding this field.
   final Function? fromJson;
+
   /// A custom function to use when encoding this field.
   final Function? toJson;
+
   /// When decoding an enum, the value to use when the value does not match any enums.
   final Enum? unknownEnumValue;
+
   /// The default value to use when the JSON value is `null`.
   final Object? defaultValue;
 
@@ -47,4 +56,12 @@ class JsonValue {
   final dynamic value;
 
   const JsonValue(this.value);
+}
+
+/// Allows you to specify a default value for a field.
+@Target({TargetKind.parameter})
+class Default {
+  final Object? value;
+
+  const Default(this.value);
 }
