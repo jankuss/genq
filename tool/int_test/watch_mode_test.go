3bf859be48cb37f8cdc3d7ad6d4420db27f0622a
Jan <jan@kuss.dev>
2024-09-25 21:44:20 +0200

    Improve watch mode test

--- tool/int_test/watch_mode_test.go
+++ tool/int_test/watch_mode_test.go
@@ -24,7 +24,8 @@ func TestWatchMode(t *testing.T) {
 		cmd.Run()
 	}()
 
-	time.Sleep(1 * time.Second)
+	// TODO: Is there a better way instead of sleeping an arbitrary amount?
+	time.Sleep(2 * time.Second)
 	addGenqTestFile("sut/changing_file.dart", "Class3")
 	addGenqTestFile("sut/changing_file2.dart", "Class4")
 	addGenqTestFile("sut/changing_file3.dart", "Class5")
@@ -32,10 +33,10 @@ func TestWatchMode(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Did not expect error: %s", err)
 	}
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	addGenqTestFile("sut/subdir/changing_file4.dart", "Class6")
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	cmd.Process.Kill()
 	w.Close()
@@ -62,4 +63,8 @@ func TestWatchMode(t *testing.T) {
 		t.Fatalf("Expected output to contain 1x 'sut/subdir/changing_file4.genq.dart'")
 	}
 
+	assertFileWithContent(t, "sut/changing_file.genq.dart")
+	assertFileWithContent(t, "sut/changing_file2.genq.dart")
+	assertFileWithContent(t, "sut/changing_file3.genq.dart")
+	assertFileWithContent(t, "sut/subdir/changing_file4.genq.dart")
 }
