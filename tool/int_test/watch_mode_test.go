9b7fe44f831c5fde64a8dcd553aeff04b73954a3
Jan <jan@kuss.dev>
2024-09-25 21:15:45 +0200

    Add int test for watch mode

---
+++ tool/int_test/watch_mode_test.go
@@ -0,0 +1,65 @@
+package inttest
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestWatchMode(t *testing.T) {
+	setup()
+	defer teardown()
+
+	addGenqTestFile("sut/changing_file.dart", "Class1")
+	addGenqTestFile("sut/changing_file2.dart", "Class2")
+
+	cmd := exec.Command(os.Getenv("GENQ_PATH"), "--watch", "--input", "sut")
+	r, w, _ := os.Pipe()
+	cmd.Stdout = w
+
+	go func() {
+		cmd.Run()
+	}()
+
+	time.Sleep(1 * time.Second)
+	addGenqTestFile("sut/changing_file.dart", "Class3")
+	addGenqTestFile("sut/changing_file2.dart", "Class4")
+	addGenqTestFile("sut/changing_file3.dart", "Class5")
+	err := os.MkdirAll("sut/subdir", 0755)
+	if err != nil {
+		t.Fatalf("Did not expect error: %s", err)
+	}
+	time.Sleep(1 * time.Second)
+
+	addGenqTestFile("sut/subdir/changing_file4.dart", "Class6")
+	time.Sleep(1 * time.Second)
+
+	cmd.Process.Kill()
+	w.Close()
+	out, _ := io.ReadAll(r)
+
+	content := string(out)
+	if !strings.Contains(content, "Watching for changes in: sut") {
+		t.Fatalf("Expected output to contain info about watching")
+	}
+
+	if strings.Count(content, "Generated sut/changing_file.genq.dart") != 2 {
+		t.Fatalf("Expected output to contain 2x 'sut/changing_file.genq.dart'")
+	}
+
+	if strings.Count(content, "Generated sut/changing_file2.genq.dart") != 2 {
+		t.Fatalf("Expected output to contain 2x 'sut/changing_file2.genq.dart'")
+	}
+
+	if strings.Count(content, "Generated sut/changing_file3.genq.dart") != 1 {
+		t.Fatalf("Expected output to contain 1x 'sut/changing_file3.genq.dart'")
+	}
+
+	if strings.Count(content, "Generated sut/subdir/changing_file4.genq.dart") != 1 {
+		t.Fatalf("Expected output to contain 1x 'sut/subdir/changing_file4.genq.dart'")
+	}
+
+}
